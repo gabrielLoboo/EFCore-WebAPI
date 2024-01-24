174f490f536610f75388e898c45da86b2abfc58d
gabrielLoboo <gabrielfrancisolobo@gmail.com>
2024-01-24 02:45:13 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication2/Data/HeroiContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<HeroiBatalha> HeroiBatalhas { get; set; }
+        public DbSet<IdentidadeSecreta> identidadeSecretas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Password=180105;Persist Security Info=True;User ID=sa;Initial Catalog=HeroApp;Data Source=DESKTOP-BFHMBTJ;TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity =>
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroiId });
+            });
+        }
+    }
+}
---
+++ WebApplication2/Models/IdentidadeSecreta.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication2.Models
+{
+    public class IdentidadeSecreta
+    {
+        public int Id { get; set; }
+        public int NomeReal { get; set; }
+        public int HeroiId { get; set; }
+        public Heroi Heroi { get; set; }
+
+    }
+}
